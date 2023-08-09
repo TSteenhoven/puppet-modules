bb33a22f084bf055078a0282db8f6cb53e17d346
DevSysEngineer <info@kvansteijn.nl>
2023-08-09 14:43:21 +0200

    Added ssh module

---
+++ ssh/manifests/init.pp
@@ -0,0 +1,43 @@
+
+class ssh(
+        $port                       = 22,
+        $permit_root_login          = false,
+        $password_authentication    = false,
+        $allow_users                = []
+    ) {
+
+    /* Required packages for SSHD */
+    package { ['openssh-server', 'openssh-client']:
+        ensure => installed
+    }
+
+    /* Convert array to string */
+    $str_allow_users = join($allow_users, ' ')
+
+    /* Create SSHD directory config */
+    file { '/etc/ssh/sshd_config.d':
+        ensure  => directory,
+        mode    => '0600',
+        owner   => 'root',
+        group   => 'root',
+        require => Package['openssh-server']
+    }
+
+    /* Create SSHD custom config */
+    file { '/etc/ssh/sshd_config.d/custom.conf':
+        ensure  => file,
+        mode    => '0600',
+        owner   => 'root',
+        group   => 'root',
+        content => template('ssh/custom.conf'),
+        require => File['/etc/ssh/sshd_config.d']
+    }
+
+    /* Ensure that ssh is always running */
+    service { 'ssh':
+        ensure      => running,
+        enable      => true,
+        require     => File['/etc/ssh/sshd_config'],
+        subscribe   => File['/etc/ssh/sshd_config']
+    }
+}
