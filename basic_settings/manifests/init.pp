492bd6ee4a557e88b3e396439a0b346e7f3fa96e
DevSysEngineer <info@kvansteijn.nl>
2023-10-20 23:46:09 +0200

    Try to fix escape

--- basic_settings/manifests/init.pp
+++ basic_settings/manifests/init.pp
@@ -745,7 +745,7 @@ class basic_settings(
             service     => {
                 'Type'      => 'oneshot',
                 'User'      => 'puppet',
-                'ExecStart' => "/usr/bin/bash -c '/usr/bin/find /var/lib/puppetserver/reports -type f -name \*.yaml -ctime +1 -delete'",
+                'ExecStart' => '/usr/bin/find /var/lib/puppetserver/reports -type f -name \\*.yaml -ctime +1 -delete',
                 'Nice'      => '19',
             },
         }
