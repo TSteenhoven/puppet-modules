4d53122879f2e89d6dc94491e1928f4d1578242c
DevSysEngineer <info@kvansteijn.nl>
2023-10-20 23:55:38 +0200

    Enable service for timer

--- basic_settings/manifests/systemd_timer.pp
+++ basic_settings/manifests/systemd_timer.pp
@@ -8,10 +8,17 @@ define basic_settings::systemd_timer(
         }
     ) {
 
+    /* Create timer file */
     file { "/etc/systemd/system/${title}.timer":
         ensure  => $ensure,
         content => template('basic_settings/systemd/timer'),
         mode    => '0644',
         notify  => Exec['systemd_daemon_reload']
+    }
+
+    /* Enable timer */
+    service {  "${title}.timer":
+        ensure  => true,
+        enable  => true
     }
 }
