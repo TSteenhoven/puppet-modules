e1b1b5510975a9da647f4004bef7bbe07b07a0f7
DevSysEngineer <info@kvansteijn.nl>
2024-06-14 13:08:23 +0200

    Try to fix apt update

--- basic_settings/manifests/package_rabbitmq.pp
+++ basic_settings/manifests/package_rabbitmq.pp
@@ -9,42 +9,10 @@ class basic_settings::package_rabbitmq(
         refreshonly => true
     }
 
-    /* Get distribution name */
-    case $os_parent {
-        'ubuntu': {
-            case $os_name {
-                'noble', 'lunar', 'jammy': {
-                    $distribution = 'jammy'
-                    $allow = true
-                }
-                default: {
-                    $distribution = ''
-                    $allow = false
-                }
-            }
-        }
-        'debian': {
-            case $os_name {
-                'buster', 'bullseye': {
-                    $distribution = 'buster'
-                    $allow = true
-                }
-                default: {
-                    $distribution = ''
-                    $allow = false
-                }
-            }
-        }
-        default: {
-            $distribution = ''
-            $allow = false
-        }
-    }
-
-    if ($enable and $allow) {
+    if ($enable) {
         /* Install Rabbitmq erlang repo */
         exec { 'package_rabbitmq_erlang':
-            command     => "printf \"deb [signed-by=/usr/share/keyrings/rabbitmq-erlang.gpg] https://ppa1.novemberain.com/rabbitmq/rabbitmq-erlang/deb/ubuntu ${distribution} main\\ndeb [signed-by=/usr/share/keyrings/rabbitmq-erlang.gpg] https://ppa2.novemberain.com/rabbitmq/rabbitmq-erlang/deb/ubuntu jammy main\\n\" > /etc/apt/sources.list.d/rabbitmq-erlang.list; curl -s https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key | gpg --dearmor | sudo tee /usr/share/keyrings/rabbitmq-erlang.gpg >/dev/null",
+            command     => "printf \"deb [signed-by=/usr/share/keyrings/rabbitmq-erlang.gpg] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/deb/${os_parent} ${os_name} main\\n\" > /etc/apt/sources.list.d/rabbitmq-erlang.list; curl -s https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/gpg.E495BB49CC4BBE5B.key | gpg --dearmor | sudo tee /usr/share/keyrings/rabbitmq-erlang.gpg >/dev/null",
             unless      => '[ -e /etc/apt/sources.list.d/rabbitmq-erlang.list ]',
             notify      => Exec['package_rabbitmq_source_list_reload'],
             require     => [Package['curl'], Package['gnupg']]
@@ -52,7 +20,7 @@ class basic_settings::package_rabbitmq(
 
         /* Install Rabbitmq server repo */
         exec { 'package_rabbitmq_server':
-            command     => "printf \"deb [signed-by=/usr/share/keyrings/rabbitmq-server.gpg] https://ppa1.novemberain.com/rabbitmq/rabbitmq-server/deb/ubuntu ${distribution} main\\ndeb [signed-by=/usr/share/keyrings/rabbitmq-server.gpg] https://ppa2.novemberain.com/rabbitmq/rabbitmq-server/deb/ubuntu jammy main\\n\" > /etc/apt/sources.list.d/rabbitmq-server.list; curl -s https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-server.9F4587F226208342.key | gpg --dearmor | sudo tee /usr/share/keyrings/rabbitmq-server.gpg >/dev/null",
+            command     => "printf \"deb [signed-by=/usr/share/keyrings/rabbitmq-server.gpg] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/deb/${os_parent} ${os_name} main\\n\" > /etc/apt/sources.list.d/rabbitmq-server.list; curl -s https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/gpg.9F4587F226208342.key | gpg --dearmor | sudo tee /usr/share/keyrings/rabbitmq-server.gpg >/dev/null",
             unless      => '[ -e /etc/apt/sources.list.d/rabbitmq-server.list ]',
             notify      => Exec['package_rabbitmq_source_list_reload'],
             require     => [Package['curl'], Package['gnupg']]
