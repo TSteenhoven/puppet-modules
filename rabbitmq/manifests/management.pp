01d9943b6630d250fc26b70ffe508b012c19d04f
DevSysEngineer <info@kvansteijn.nl>
2024-06-17 10:11:54 +0200

    Improve rabbitmq

--- rabbitmq/manifests/management.pp
+++ rabbitmq/manifests/management.pp
@@ -2,11 +2,12 @@ class rabbitmq::management(
         $ssl_ca_certificate     = undef,
         $ssl_certificate        = undef,
         $ssl_certificate_key    = undef,
-    ){
+        $ssl_port               = 15671,
+    ) {
 
     /* Setup the plugin */
     exec { 'rabbitmq_management_plugin':
-        command =>  '(umask 27 && rabbitmq-plugins --quiet enable rabbitmq_management)',
+        command =>  'bash -c "(umask 27 && rabbitmq-plugins --quiet enable rabbitmq_management)"',
         unless  => 'rabbitmq-plugins --quiet is_enabled rabbitmq_management',
         require => Package['rabbitmq-server']
     }
@@ -14,8 +15,19 @@ class rabbitmq::management(
     /* Check if all cert variables are given */
     if ($ssl_ca_certificate != undef and $ssl_certificate != undef and $ssl_certificate_key != undef) {
         $https_allow = true
+        $ssl_ca_certificate_correct = $ssl_ca_certificate
+        $ssl_certificate_correct = $ssl_certificate
+        $ssl_certificate_key_correct = $ssl_certificate_key
+    } elsif ($rabbitmq::tls::ssl_ca_certificate != undef and $rabbitmq::tls::ssl_certificate != undef and $rabbitmq::tls::ssl_certificate_key != undef) {
+        $https_allow = true
+        $ssl_ca_certificate_correct = $rabbitmq::tls::ssl_ca_certificate
+        $ssl_certificate_correct = $rabbitmq::tls::ssl_certificate
+        $ssl_certificate_key_correct = $rabbitmq::tls::ssl_certificate_key
     } else {
         $https_allow = false
+        $ssl_ca_certificate_correct = undef
+        $ssl_certificate_correct = undef
+        $ssl_certificate_key_correct = undef
     }
 
     /* Create management config file */
---
+++ rabbitmq/manifests/tls.pp
@@ -0,0 +1,29 @@
+class rabbitmq::management(
+        $tcp_enable             = false,
+        $tcp_port               = 5672,
+        $ssl_ca_certificate     = undef,
+        $ssl_certificate        = undef,
+        $ssl_certificate_key    = undef,
+        $ssl_port               = 5671
+    ) {
+
+    /* Check if all cert variables are given */
+    if ($ssl_ca_certificate != undef and $ssl_certificate != undef and $ssl_certificate_key != undef) {
+        $tls_allow = true
+        $tcp_enable_correct = $tcp_enable
+    } else {
+        $tls_allow = false
+        $tcp_enable_correct = true
+    }
+
+    /* Create management config file */
+    file { '/etc/rabbitmq/conf.d/tls.conf':
+        ensure  => file,
+        content => template('rabbitmq/tls.conf'),
+        owner   => 'rabbitmq',
+        group   => 'rabbitmq',
+        mode    => '0600',
+        notify  => Service['rabbitmq-server'],
+        require => File['rabbitmq_config_dir']
+    }
+}
