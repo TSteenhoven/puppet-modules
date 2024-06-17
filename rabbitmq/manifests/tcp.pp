cd9a157bf22660c0babe6a2969e2a50efb7d494f
DevSysEngineer <info@kvansteijn.nl>
2024-06-17 11:43:36 +0200

    Use other ciphers

--- rabbitmq/manifests/tcp.pp
+++ rabbitmq/manifests/tcp.pp
@@ -7,23 +7,15 @@ class rabbitmq::tcp(
         $ssl_port               = 5671,
         $ssl_protocols          = ['tlsv1.3', 'tlsv1.2'],
         $ssl_ciphers = [
-            'TLS_AES_256_GCM_SHA384',
-            'TLS_AES_128_GCM_SHA256',
-            'TLS_CHACHA20_POLY1305_SHA256',
-            'TLS_AES_128_CCM_SHA256',
-            'TLS_AES_128_CCM_8_SHA256',
-            'ECDHE-ECDSA-AES256-GCM-SHA384',
-            'ECDHE-RSA-AES256-GCM-SHA384',
-            'ECDH-ECDSA-AES256-GCM-SHA384',
-            'ECDH-RSA-AES256-GCM-SHA384',
-            'DHE-RSA-AES256-GCM-SHA384',
-            'DHE-DSS-AES256-GCM-SHA384',
             'ECDHE-ECDSA-AES128-GCM-SHA256',
             'ECDHE-RSA-AES128-GCM-SHA256',
-            'ECDH-ECDSA-AES128-GCM-SHA256',
-            'ECDH-RSA-AES128-GCM-SHA256',
+            'ECDHE-ECDSA-AES256-GCM-SHA384',
+            'ECDHE-RSA-AES256-GCM-SHA384',
+            'ECDHE-ECDSA-CHACHA20-POLY1305',
+            'ECDHE-RSA-CHACHA20-POLY1305',
             'DHE-RSA-AES128-GCM-SHA256',
-            'DHE-DSS-AES128-GCM-SHA256'
+            'DHE-RSA-AES256-GCM-SHA384',
+            'DHE-RSA-CHACHA20-POLY1305'
         ]
     ) {
 
