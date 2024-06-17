bd1b11b7e0621588fd8208b2cf8e0b8c80f1c7a9
DevSysEngineer <info@kvansteijn.nl>
2024-06-17 12:06:14 +0200

    Try to fix ciphers

--- rabbitmq/manifests/tcp.pp
+++ rabbitmq/manifests/tcp.pp
@@ -7,15 +7,18 @@ class rabbitmq::tcp(
         $ssl_port               = 5671,
         $ssl_protocols          = ['tlsv1.3', 'tlsv1.2'],
         $ssl_ciphers = [
-            'ECDHE-ECDSA-AES128-GCM-SHA256',
+            'TLS_AES_256_GCM_SHA384',
+            'TLS_AES_128_GCM_SHA256',
+            'TLS_CHACHA20_POLY1305_SHA256',
+            'TLS_AES_128_CCM_SHA256',
+            'TLS_AES_128_CCM_8_SHA256',
+            'ECDHE-ECDSA-AES128-CCM',
+            'ECDHE-ECDSA-AES128-CCM8',
+            'ECDHE-ECDSA-AES256-CCM',
+            'ECDHE-ECDSA-AES256-CCM8',
             'ECDHE-RSA-AES128-GCM-SHA256',
-            'ECDHE-ECDSA-AES256-GCM-SHA384',
             'ECDHE-RSA-AES256-GCM-SHA384',
-            'ECDHE-ECDSA-CHACHA20-POLY1305',
-            'ECDHE-RSA-CHACHA20-POLY1305',
-            'DHE-RSA-AES128-GCM-SHA256',
-            'DHE-RSA-AES256-GCM-SHA384',
-            'DHE-RSA-CHACHA20-POLY1305'
+            'ECDHE-RSA-CHACHA20-POLY1305'
         ]
     ) {
 
