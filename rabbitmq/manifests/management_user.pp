694d49b2e9d9dd4830cd4fa5bbe657be0dfa978b
DevSysEngineer <info@kvansteijn.nl>
2024-06-18 13:31:06 +0200

    Improved rabbitmq user

--- rabbitmq/manifests/management_user.pp
+++ rabbitmq/manifests/management_user.pp
@@ -8,29 +8,29 @@ define rabbitmq::management_user(
         present: {
             /* When password is not given; Create random passowrd */
             if ($password == undef) {
-                if (defined(User[$name])) {
-                    $user_addd = "TMPPASS=`/usr/bin/pwgen -s 26 1`; echo \$TMPPASS > /home/${name}/.rabbitmq.password; chown ${name}:${name} /home/${name}/.rabbitmq.password; chmod 600 /home/${name}/.rabbitmq.password; echo \$TMPPASS | /usr/sbin/rabbitmqctl add_user ${name}"
+                if (defined(User[$title])) {
+                    $user_addd = "TMPPASS=`/usr/bin/pwgen -s 26 1`; echo \$TMPPASS > /home/${title}/.rabbitmq.password; chown ${title}:${title} /home/${title}/.rabbitmq.password; chmod 600 /home/${title}/.rabbitmq.password; echo \$TMPPASS | /usr/sbin/rabbitmqctl add_user ${title}"
                 } else {
                     fail('User not present')
                 }
             } else {
-                $user_addd = "/usr/sbin/rabbitmqctl add_user ${name} ${password}"
+                $user_addd = "/usr/sbin/rabbitmqctl add_user ${title} ${password}"
             }
 
             /* Create user */
-            exec { "rabbitmq_management_user_${name}":
+            exec { "rabbitmq_management_user_${title}":
                 command => $user_addd,
-                unless  => "/usr/sbin/rabbitmqctl list_user_limits --user ${name}",
+                unless  => "/usr/sbin/rabbitmqctl list_user_limits --user ${title}",
                 require => Exec['rabbitmq_management_plugin']
             }
 
             /* Set tags */
             $user_tags_join = join($tags, ' ')
             $user_tags_search = join($tags, ' | grep ')
-            exec { "rabbitmq_management_user_${name}_tags":
-                command => "/usr/sbin/rabbitmqctl --quiet set_user_tags ${name} ${user_tags_join}",
-                unless  => "/usr/sbin/rabbitmqctl --quiet list_users --no-table-headers | grep ${name} | cut -f2 | grep ${user_tags_search}",
-                require => Exec["rabbitmq_management_user_${name}"]
+            exec { "rabbitmq_management_user_${title}_tags":
+                command => "/usr/sbin/rabbitmqctl --quiet set_user_tags ${title} ${user_tags_join}",
+                unless  => "/usr/sbin/rabbitmqctl --quiet list_users --no-table-headers | grep ${title} | cut -f2 | grep ${user_tags_search}",
+                require => Exec["rabbitmq_management_user_${title}"]
             }
         }
         absent: {
