7e915d64553ba2ee33cd2d0e15624fd3e26d6e73
DevSysEngineer <info@kvansteijn.nl>
2024-08-19 23:13:34 +0200

    Cleanup some code

--- basic_settings/templates/login/login-notify.sh
+++ basic_settings/templates/login/login-notify.sh
@@ -1,11 +1,21 @@
+#!/bin/sh
 # Managed by puppet
 
 # Check if mail is available
 MAIL=/usr/bin/mail
 test -x $MAIL || exit 1
 
+# Check if date is available
+DATE=/usr/bin/date
+test -x $DATE || exit 1
+
+# Check if whoami is available
+WHOAMI=/usr/bin/whoami
+test -x $WHOAMI || exit 1
+
 # Current date
-NOW=$(date)
+NOW=$($DATE)
+CURRENT_USER=${$WHOAMI}
 
 # Try to get IP
 if [ -n "$(echo $SSH_CLIENT)" ]; then
@@ -17,17 +27,17 @@ else
 fi
 
 # Chec if we are in interactive shell
-if [ -n "$PS1" ]; then
-    # Show message 
-    if [ "$USER" = "root" ]; then
+if [[ "$0" =~ ^- ]]; then
+    if [[ "$CURRENT_USER" = "root" ]]; then
         printf "\033[0;31mYou login as root, this action is registered and sent to the server administrator(s).\033[0m\n"
-        printf '%b\n' "User $USER logged in as root into the <%= @server_fdqn %> at $NOW\nIP: $IP" | $MAIL -s "Audit root login $USER" -r "audit@<%= @server_fdqn %>" "<%= @mail_to %>"
-        exit 0
-    fi
-
-    # Show message 
-    printf "\033[0;36mYour IP (%s), login time (%s) and username (%s) have been registered and sent to the server administrator(s).\033[0m\n" "$IP" "$NOW" "$USER"
+    else
+        printf "\033[0;36mYour IP (%s), login time (%s) and username (%s) have been registered and sent to the server administrator(s).\033[0m\n" "$IP" "$NOW" "$USER"
+    fi    
 fi
 
 # Send message
-printf '%b\n' "User $USER logged into the <%= @server_fdqn %> at $NOW\nIP: $IP" | $MAIL -s "Audit login $USER" -r "audit@<%= @server_fdqn %>" "<%= @mail_to %>"
+if [ "$CURRENT_USER" = "root" ]; then
+    printf '%b\n' "User $USER logged in as root into the <%= @server_fdqn %> at $NOW\nIP: $IP" | $MAIL -s "Audit root login $USER" -r "audit@<%= @server_fdqn %>" "<%= @mail_to %>"
+else
+    printf '%b\n' "User $USER logged into the <%= @server_fdqn %> at $NOW\nIP: $IP" | $MAIL -s "Audit login $USER" -r "audit@<%= @server_fdqn %>" "<%= @mail_to %>"
+fi
